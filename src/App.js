69d5b53e96e20318fa9a4d1506e68ec64cdadd2b
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-03-31 15:18:23 +0300

    UpperCase

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <div>
           <h2>Counter:</h2>
           <h1>{count}</h1>
-          <button onClick={() => setCount(count -1)} className="minus">- Minus</button>
-          <button onClick={() => setCount(count +1)} className="plus">Plus +</button>
+          <button onClick={() => setCount(count -1)} className="minus">- MINUS</button>
+          <button onClick={() => setCount(count +1)} className="plus">PLUS +</button>
         </div>
       </div>
   );
